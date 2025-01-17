334080407746a6bdc5aa5ecec85f8957cc5228c5
AlinaTPF <alina.hernandez@tpfingenieria.com>
2025-01-17 12:46:43 +0100

    Added backend url from Render

--- src/api/task.api.js
+++ src/api/task.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const tasksApi = axios.create({
-  baseURL: "http://127.0.0.1:8000/task/api/v1/tasks/",
+  baseURL: "https://djangoback-q83s.onrender.com/task/api/v1/tasks/",
 });
 
 export const getAllTasks = () => tasksApi.get("/");
